a1c7096dff76aad1cc5589daed30ee5638c6174c
dhptrungit22 <50943955+dinhhuuphuctrung@users.noreply.github.com>
2020-11-27 21:21:30 +0700

    Add files via upload

---
+++ ColorTransformer.cpp
@@ -0,0 +1,32 @@
+﻿// ColorTransformer.cpp
+#include "ColorTransformer.h"
+
+// Hàm nhận vào một ảnh, thay đổi độ sáng của ảnh này và lưu kết quả vào ảnh mới
+int ColorTransformer::ChangeBrighness(const Mat& sourceImage, Mat& destinationImage, short b) {
+
+}
+
+// Hàm nhận vào một ảnh, thay đổi độ tương phản của ảnh này và lưu kết quả vào ảnh mới
+int ColorTransformer::ChangeContrast(const Mat& sourceImage, Mat& destinationImage, float c) {
+
+}
+
+// Hàm tính lược đồ màu tổng quát cho ảnh bất kỳ
+int ColorTransformer::CalcHistogram(const Mat& sourceImage, Mat& histMatrix) {
+
+}
+
+// Hàm cân bằng lược đồ màu tổng quát cho ảnh bất kỳ
+int ColorTransformer::HistogramEqualization(const Mat& sourceImage, Mat& destinationImage) {
+
+}
+
+// Hàm cân bằng lược đồ màu tổng quát cho ảnh bất kỳ
+int ColorTransformer::DrawHistogram(const Mat& histMatrix, Mat& histImage) {
+
+}
+
+// Hàm so sánh hai ảnh
+float ColorTransformer::CompareImage(const Mat& image1, Mat& image2) {
+
+}
---
+++ Converter.cpp
@@ -0,0 +1,154 @@
+﻿// Converter.cpp
+#include "Converter.h"
+
+// Hàm chuyển đổi không gian màu của ảnh từ RGB sang GrayScale
+int Converter::RGB2GrayScale(const Mat& sourceImage, Mat& destinationImage) 
+{
+	if (sourceImage.data == NULL)
+		return 1;
+
+	int width = sourceImage.cols;
+	int height = sourceImage.rows;
+	int srcChannels = sourceImage.channels();
+
+	destinationImage = Mat(height, width, CV_8UC1);
+	int dstChannels = destinationImage.channels();
+
+	for (int y = 0; y < height; y++) 
+	{
+		const uchar* pSrcRow = sourceImage.ptr<uchar>(y);
+		uchar* pDstRow = destinationImage.ptr<uchar>(y);
+
+		for (int x = 0; x < width; x++, pSrcRow += srcChannels, pDstRow += dstChannels) 
+		{
+			uchar B = pSrcRow[0];
+			uchar G = pSrcRow[1];
+			uchar R = pSrcRow[2];
+			uchar gray_val = uchar(0.3 * R + 0.59 * G + 0.11 * B);
+			pDstRow[0] = gray_val;
+		}
+	}
+
+	return 0;
+}
+
+// Hàm chuyển đổi không gian màu của ảnh từ GrayScale sang RGB
+int Converter::GrayScale2RGB(const Mat& sourceImage, Mat& destinationImage) 
+{
+	if (sourceImage.data == NULL)
+		return 1;
+
+	int width = sourceImage.cols;
+	int height = sourceImage.rows;
+	int srcChannels = sourceImage.channels();
+
+	destinationImage = Mat(height, width, CV_8UC3);
+	int dstChannels = destinationImage.channels();
+
+	for (int y = 0; y < height; y++) 
+	{
+		const uchar* pSrcRow = sourceImage.ptr<uchar>(y);
+		uchar* pDstRow = destinationImage.ptr<uchar>(y);
+
+		for (int x = 0; x < width; x++, pSrcRow += srcChannels, pDstRow += dstChannels) 
+		{
+			uchar gray_val = pSrcRow[0];
+			pDstRow[0] = gray_val;
+			pDstRow[1] = gray_val;
+			pDstRow[2] = gray_val;
+		}
+	}
+
+	return 0;
+}
+
+// Hàm chuyển đổi không gian màu của ảnh từ RGB sang HSV
+int Converter::RGB2HSV(const Mat& sourceImage, Mat& destinationImage) {
+	if (sourceImage.data == NULL)
+		return 1;
+
+	int width = sourceImage.cols;
+	int height = sourceImage.rows;
+	int srcChannels = sourceImage.channels();
+
+	destinationImage = Mat(height, width, CV_8UC3);
+	int dstChannels = destinationImage.channels();
+
+	for (int y = 0; y < height; y++)
+	{
+		const uchar* pSrcRow = sourceImage.ptr<uchar>(y);
+		uchar* pDstRow = destinationImage.ptr<uchar>(y);
+
+		for (int x = 0; x < width; x++, pSrcRow += srcChannels, pDstRow += dstChannels)
+		{
+			uchar B = pSrcRow[0];
+			uchar G = pSrcRow[1];
+			uchar R = pSrcRow[2];
+
+			// (0, 1)
+			double r = double(R / 255);
+			double g = double(G / 255);
+			double b = double(B / 255);
+
+			double min, max, delta;
+			double h, s, v;
+
+			// min(r, g, b)
+			min = r < g ? r : g;
+			min = min < b ? min : b;
+
+			// max(r, g, b)
+			max = r > g ? r : g;
+			max = max > b ? max : b;
+
+			delta = max - min;
+
+			v = max;
+			if (delta > 0)
+			{
+				if (max == r)
+					h = (g - b) / delta;
+				else if (max == g)
+					h = 2.0 + (b - r) / delta;
+				else
+					h = 4.0 + (r - g) / delta;
+
+				if (max > 0)
+					s = delta / max;
+				else
+					s = 0;
+			}
+			else
+			{
+				s = 0;
+				h = NAN;
+			}
+
+			h *= 60;
+			if (h < 0.0)
+				h += 360;
+
+			//
+			pDstRow[0] = uchar(v);
+			pDstRow[1] = uchar(s);
+			pDstRow[2] = uchar(h);
+		}
+	}
+
+	return 0;
+}
+
+// Hàm chuyển đổi không gian màu của ảnh từ HSV sang RGB
+int Converter::HSV2RGB(const Mat& sourceImage, Mat& destinationImage) {
+
+}
+
+
+// Hàm chuyển đổi không gian màu của ảnh
+int Converter::Convert(Mat& sourceImage, Mat& destinationImage, int type) {
+
+}
+
+int Converter::Convert(IplImage* sourceImage, IplImage* destinationImage, int type) {
+
+}
