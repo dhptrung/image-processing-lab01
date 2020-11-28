6274a1152406b45f1724cf35e3395da2faecf74c
dhptrungit22 <50943955+dinhhuuphuctrung@users.noreply.github.com>
2020-11-29 00:16:31 +0700

    Update Converter.cpp

--- Converter.cpp
+++ Converter.cpp
@@ -4,16 +4,20 @@
 // Hàm chuyển đổi không gian màu của ảnh từ RGB sang GrayScale
 int Converter::RGB2GrayScale(const Mat& sourceImage, Mat& destinationImage) 
 {
+	// Kiểm tra sourceImage lỗi
 	if (sourceImage.data == NULL)
 		return 1;
-
+	
+	// Các thông số kích thước ảnh sourceImage
 	int width = sourceImage.cols;
 	int height = sourceImage.rows;
 	int srcChannels = sourceImage.channels();
 
-	destinationImage = Mat(height, width, CV_8UC1);
+	// Tạo destinationImage có thông số thích hợp
+	destinationImage = Mat(height, width, CV_8UC1, Scalar(0));
 	int dstChannels = destinationImage.channels();
 
+	// Dùng con trỏ đầu dòng để truy xuất đến từng pixel
 	for (int y = 0; y < height; y++) 
 	{
 		const uchar* pSrcRow = sourceImage.ptr<uchar>(y);
@@ -21,10 +25,14 @@ int Converter::RGB2GrayScale(const Mat& sourceImage, Mat& destinationImage)
 
 		for (int x = 0; x < width; x++, pSrcRow += srcChannels, pDstRow += dstChannels) 
 		{
+			// Lấy thông số R, G, B (uchar) từ sourceImage
 			uchar B = pSrcRow[0];
 			uchar G = pSrcRow[1];
 			uchar R = pSrcRow[2];
+			
+			// Tính giá trị gray 
 			uchar gray_val = uchar(0.3 * R + 0.59 * G + 0.11 * B);
+			// Gán giá trị gray cho pixel của destinationImage
 			pDstRow[0] = gray_val;
 		}
 	}
@@ -35,16 +43,20 @@ int Converter::RGB2GrayScale(const Mat& sourceImage, Mat& destinationImage)
 // Hàm chuyển đổi không gian màu của ảnh từ GrayScale sang RGB
 int Converter::GrayScale2RGB(const Mat& sourceImage, Mat& destinationImage) 
 {
+	// Kiểm tra sourceImage lỗi
 	if (sourceImage.data == NULL)
 		return 1;
 
+	// Các thông số kích thước ảnh sourceImage
 	int width = sourceImage.cols;
 	int height = sourceImage.rows;
 	int srcChannels = sourceImage.channels();
 
-	destinationImage = Mat(height, width, CV_8UC3);
+	// Tạo destinationImage có thông số thích hợp
+	destinationImage = Mat(height, width, CV_8UC3, Scalar(0));
 	int dstChannels = destinationImage.channels();
 
+	// Dùng con trỏ đầu dòng để truy xuất đến từng pixel
 	for (int y = 0; y < height; y++) 
 	{
 		const uchar* pSrcRow = sourceImage.ptr<uchar>(y);
@@ -52,10 +64,12 @@ int Converter::GrayScale2RGB(const Mat& sourceImage, Mat& destinationImage)
 
 		for (int x = 0; x < width; x++, pSrcRow += srcChannels, pDstRow += dstChannels) 
 		{
+			// Lấy giá trị gray từ sourceImage (ảnh GrayScale)
 			uchar gray_val = pSrcRow[0];
-			pDstRow[0] = gray_val;
-			pDstRow[1] = gray_val;
-			pDstRow[2] = gray_val;
+			// Gán giá trị gray cho từng channel B, R, G của pixel destinationImage
+			pDstRow[0] = gray_val; // B
+			pDstRow[1] = gray_val; // G
+			pDstRow[2] = gray_val; // R
 		}
 	}
 
@@ -85,69 +99,6 @@ int Converter::RGB2HSV(const Mat& sourceImage, Mat& destinationImage)
 	// Tạo destinationImage có thông số thích hợp
 	destinationImage = Mat(height, width, CV_32FC3, Scalar(0));
 	int dstChannels = destinationImage.channels();
-
-	/*
-	for (int y = 0; y < height; y++)
-	{
-		const uchar* pSrcRow = sourceImage.ptr<uchar>(y);
-		float* pDstRow = destinationImage.ptr<float>(y);
-
-		for (int x = 0; x < width; x++, pSrcRow += srcChannels, pDstRow += dstChannels)
-		{
-			uchar B = pSrcRow[0];
-			uchar G = pSrcRow[1];
-			uchar R = pSrcRow[2];
-
-			// (0, 1)
-			double r = double(R / 255.0);
-			double g = double(G / 255.0);
-			double b = double(B / 255.0);
-
-			double min, max, delta;
-			double h, s, v;
-
-			// min(r, g, b)
-			min = r < g ? r : g;
-			min = min < b ? min : b;
-
-			// max(r, g, b)
-			max = r > g ? r : g;
-			max = max > b ? max : b;
-
-			delta = max - min;
-
-			v = max;
-			if (delta > 0)
-			{
-				if (max == r)
-					h = (g - b) / delta;
-				else if (max == g)
-					h = 2.0 + (b - r) / delta;
-				else
-					h = 4.0 + (r - g) / delta;
-
-				if (max > 0)
-					s = delta / max;
-				else
-					s = 0;
-			}
-			else
-			{
-				s = 0;
-				h = NAN;
-			}
-
-			h *= 60;
-			if (h < 0.0)
-				h += 360;
-
-			//
-			pDstRow[0] = v;
-			pDstRow[1] = s;
-			pDstRow[2] = h;
-		}
-	}
-	*/
 
 	// Duyệt từng pixel R, G, B của sourceImage
 	// Convert sang H, S, V và lưu vào destinationImage
